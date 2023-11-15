1bcc69e2bd384d18c0de9dc26bcd8045239ea9e6
Varshith-Kali <pulivarshit@gmail.com>
2023-11-16 01:41:18 +0530

    Animation savepoint before changing overlay-div width

--- script.js
+++ script.js
@@ -19,11 +19,25 @@ document.querySelector("#overlay1").addEventListener("mousemove", function(dets)
 
 })
 
+document.querySelector("#overlay1").addEventListener("mouseleave", function(dets){
+    document.querySelector("#elem1 img").style.scale = 0
+    document.querySelector("#elem1 img").style.opacity = 0
+    cursor.style.opacity = 1
+
+})
+
 document.querySelector("#overlay2").addEventListener("mousemove", function(dets){
     document.querySelector("#elem2 img").style.scale = 1
     document.querySelector("#elem2 img").style.opacity = 1
     cursor.style.opacity = 0
     document.querySelector("#elem2 img").style.left = (dets.x - 860) + "px"
     document.querySelector("#elem2 img").style.top = (dets.y - 280) + "px"
+
+})
+
+document.querySelector("#overlay2").addEventListener("mouseleave", function(dets){
+    document.querySelector("#elem2 img").style.scale = 0
+    document.querySelector("#elem2 img").style.opacity = 0
+    cursor.style.opacity = 1
 
 })
